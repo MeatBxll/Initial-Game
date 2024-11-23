8154ca001f315b6da4943b141f4969dc0e42f6ab
MeatBxll <anthonymancini27@gmail.com>
2024-11-22 22:26:01 -0500

    Im getting boned by animations please help

--- Assets/scripts/player/slashMeleeAnimator.cs
+++ Assets/scripts/player/slashMeleeAnimator.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class slashMeleeAnimator : MonoBehaviour
+public class slashMeleeAnimator : NetworkBehaviour
 {
     public void EndSwing()
     {
