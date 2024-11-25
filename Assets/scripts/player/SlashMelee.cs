8352a351279d6846ea9bb4869844a41ab66b82a4
MeatBxll <anthonymancini27@gmail.com>
2024-11-24 23:03:30 -0500

    Animations yet again
    Issues Issues Issues

--- Assets/scripts/player/SlashMelee.cs
+++ Assets/scripts/player/SlashMelee.cs
@@ -9,14 +9,12 @@ public class SlashMelee : NetworkBehaviour
     public Animator animator;
     [SerializeField] private float SwingSpeed;
     private AnimationClip SwingClip;
-    private int SwingState;
 
     private void Start() 
     {
         animator.SetFloat("SwingSpeed", SwingSpeed);
         AnimationClip[] AnimationClips = animator.runtimeAnimatorController.animationClips;
         foreach(AnimationClip c in AnimationClips) if(c.name == "KnightSwing") SwingClip = c;
-        SwingState = 0;
     }
     void FixedUpdate()
     {
@@ -24,43 +22,14 @@ public class SlashMelee : NetworkBehaviour
         if(GameObject.FindGameObjectWithTag("playerUI").GetComponent<PlayerUI>().GamePaused) return;
         if (Input.GetMouseButton(0)) 
         {
-            if(SwingState != 0) return;
-            animator.SetFloat("Swing", 1);
-            animator.SetFloat("Swing", 0);
+            if(animator.GetBool("IsSwinging") == true) return;
+            animator.SetBool("IsSwinging", true);
             Invoke("EndSwing", SwingClip.length / SwingSpeed);
-            SwingState = 2;
         }
     }
 
-    //SwingState 0 means done swinging/not swinging
-    //SwingState 1 means is currently normalswinging
-    //SwingState 2 means transfering from not swinging to swinging or is backwardswinging
     public void EndSwing()
     {
-        if (Input.GetMouseButton(0))
-        {
-            if(SwingState == 2)
-            {
-                animator.SetFloat("SwingSpeed", -SwingSpeed);
-                animator.SetFloat("Swing", 1);
-                animator.SetFloat("Swing", 0);
-                Invoke("EndSwing", SwingClip.length / SwingSpeed);
-                SwingState = 1;
-            }
-            else
-            {
-                animator.SetFloat("SwingSpeed", SwingSpeed);
-                animator.SetFloat("Swing", 1);
-                animator.SetFloat("Swing", 0);
-                Invoke("EndSwing", SwingClip.length / SwingSpeed);
-                SwingState = 2;
-            }
-        }
-        else 
-        {
-            animator.SetFloat("Swing", 2);
-            SwingState = 0;
-        }
-        
+        animator.SetBool("IsSwinging", false);
     }
 }
