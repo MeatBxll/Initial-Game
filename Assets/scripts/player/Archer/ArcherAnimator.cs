b64d8381a8917173d3c349e425887e0acb887142
MeatBxll <anthonymancini27@gmail.com>
2024-12-21 21:33:45 -0500

    Worked on archer a little
    I also lost my note that talked about what abilities archer is going to have so i need to spend some time looking through my files to find it because it has some ideas i wanna try and work in on there

---
+++ Assets/scripts/player/Archer/ArcherAnimator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcherAnimator : MonoBehaviour
+{
+    public void BowFullyDrawnAnim()
+    {
+        
+    }
+
+    public void StopMeleeAnim()
+    {
+
+    }
+
+    public void StopVollyAnim()
+    {
+
+    }
+
+    public void StopStunArrowAnim()
+    {
+
+    }
+}
