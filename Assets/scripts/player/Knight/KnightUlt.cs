0ac503a8d872d2ec4e3a1dc1b7da7a7d242ea9ca
MeatBxll <anthonymancini27@gmail.com>
2025-01-26 21:13:16 -0500

    knight ult

--- Assets/scripts/player/Knight/KnightUlt.cs
+++ Assets/scripts/player/Knight/KnightUlt.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class KnightUlt : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject[] ultPathObjs;
+    [HideInInspector] public bool IsRedTeam;
+    [HideInInspector] public GameObject PlayerThatSpawnedSmoke;
+    private int whichBall;
+    private void Start()
     {
-        
+        //make ult objs destory faster and get deployed out of the knight with the dash
+        Transform SpawnSpot = PlayerThatSpawnedSmoke.GetComponent<Knight>().FireBallSpawnLocation.transform;
+        float smokeSpeed = PlayerThatSpawnedSmoke.GetComponent<Knight>().smokeSpeed;
+        foreach(GameObject g in ultPathObjs)
+        {
+            g.gameObject.transform.position = gameObject.transform.position + (SpawnSpot.forward*3);
+            g.gameObject.GetComponent<Rigidbody>().velocity = SpawnSpot.forward * smokeSpeed;
+            g.GetComponent<KnightSmokeObj>().playerThatSpawnedSmoke = PlayerThatSpawnedSmoke;
+        }
+        Invoke("StopBall", .3f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StopBall()
     {
-        
+        if(whichBall > ultPathObjs.Count() -1) 
+        {
+            Destroy(gameObject, PlayerThatSpawnedSmoke.GetComponent<Knight>().smokeDurration);
+            return;
+        }
+        ultPathObjs[whichBall].GetComponent<Rigidbody>().velocity = Vector2.zero;
+        whichBall ++;
+        Invoke("StopBall", .3f); 
     }
 }
