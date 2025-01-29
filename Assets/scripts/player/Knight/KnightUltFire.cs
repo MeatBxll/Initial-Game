94cf2db4ef88bed5d4fdd1fae12433a619398b7a
MeatBxll <anthonymancini27@gmail.com>
2025-01-29 17:43:27 -0500

    knight ult

--- Assets/scripts/player/Knight/KnightUltFire.cs
+++ Assets/scripts/player/Knight/KnightUltFire.cs
@@ -7,13 +7,28 @@ public class KnightUltFire : MonoBehaviour
     [HideInInspector] public GameObject playerThatSpawnedUltFire;
     private float[] passiveDmg;
     private float baseDmg;
+    private bool IsRedTeam;
+    private Transform startPos;
     void Start()
     {
         Knight g = playerThatSpawnedUltFire.GetComponent<Knight>();
+        IsRedTeam = playerThatSpawnedUltFire.GetComponent<Health>().IsRedTeam;
+
         passiveDmg = new float[2];
         passiveDmg[0] = g.FireBallElements[1];
         passiveDmg[1] = g.FireBallElements[2];
 
         baseDmg = g.KnightUltFireLeftBehindBaseDmg;
+    }
+
+    private void OnTriggerEnter(Collider obj) 
+    {
+        if(obj.tag == "Player")
+        {
+            if(obj.GetComponent<Health>().IsRedTeam == IsRedTeam) return;
+            obj.GetComponent<Health>().TakeDmg(baseDmg);
+            obj.GetComponent<Health>().TakeDmgOverTime(passiveDmg[0], passiveDmg[1], playerThatSpawnedUltFire);
+            playerThatSpawnedUltFire.GetComponent<Knight>().IncreasePassiveCount();
+        }
     }
 }
