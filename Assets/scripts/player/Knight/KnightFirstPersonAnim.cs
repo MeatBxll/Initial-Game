4eabaf75617b32cf19d3e7246b44122a9e4e747e
MeatBxll <anthonymancini27@gmail.com>
2025-01-30 19:16:06 -0500

    knight pov

--- Assets/scripts/player/Knight/KnightFirstPersonAnim.cs
+++ Assets/scripts/player/Knight/KnightFirstPersonAnim.cs
@@ -4,5 +4,50 @@ using UnityEngine;
 
 public class KnightFirstPersonAnim : MonoBehaviour
 {
+[HideInInspector] public Knight knight;
 
+    // swing stuff
+    public void StopSwingAnimPov()
+    {
+        gameObject.GetComponent<Animator>().SetBool("IsSwinging" , false);
+        gameObject.GetComponent<Animator>().SetFloat("SwingSpeed", knight.SwingSpeed);
+    }
+
+    public void ReverseSwingAnimationPov()
+    {
+        if(Input.GetMouseButton(0)) gameObject.GetComponent<Animator>().SetFloat("SwingSpeed", -knight.SwingSpeed);
+    }
+
+    public void ResetSwingPov()
+    {
+        if(!Input.GetMouseButton(0)) gameObject.GetComponent<Animator>().SetBool("IsSwinging" , false);
+        gameObject.GetComponent<Animator>().SetFloat("SwingSpeed", knight.SwingSpeed);
+    }
+
+    //fireball stuff
+    public void SpawnFireballPov()
+    {
+        knight.CmdCastFireBall(knight.gameObject.GetComponent<Health>().IsRedTeam);
+    }
+    public void StopFireBallAnim()
+    {
+        gameObject.GetComponent<Animator>().SetBool("Fireball" , false);
+        
+    }
+    //smoke stuff
+    public void StopSmokeAnimPov()
+    {
+        gameObject.GetComponent<Animator>().SetBool("Smoke" , false);
+
+    }
+    public void SpawnSmokePov()
+    {
+        knight.CmdCastSmoke();
+    }
+
+    //ult stuff
+    public void StopKnightUltAnimPov()
+    {
+
+    }
 }
