7c55cb01fabfc886602194d5d06e2a6cabb35ce7
MeatBxll <anthonymancini27@gmail.com>
2025-02-02 19:35:10 -0500

    characters
    Spent some time today mapping out new ideas for characters but i really need to finish knight out because hes been dragging on for far too long

---
+++ Assets/boomerang.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boomerang : MonoBehaviour
+{
+    
+    void Update()
+    {
+
+        // austrailian boomeranger
+        
+        // make a guy with boomerang
+
+        // throws boomer rang it comes back but has to wait for them to come back
+
+        // has 4 boomers that take a few seconds to come back for each one
+
+        // ult is a massive flaming rang that goes through walls
+
+        //cooldown 1 makes all boomerangs come back instantly 
+
+        //cooldown 2 special rang that can target enemy and stun them for a few seconds and at the end blows up for some dmg
+
+        //cooldown 3 boomerrang has a small dash in any direction 
+
+        //passive ranger is known for dealing with enemies larger than himself so he can deal more dmg to target who have more health than himself (like 5 - 10 % depending on the health difference)
+    }
+}
