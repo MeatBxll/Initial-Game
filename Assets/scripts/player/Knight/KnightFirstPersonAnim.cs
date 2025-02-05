b55f3ebe38d13e1f46bed3ea6d34f17920ed54b6
MeatBxll <anthonymancini27@gmail.com>
2025-02-04 20:25:38 -0500

    Pov animation

--- Assets/scripts/player/Knight/KnightFirstPersonAnim.cs
+++ Assets/scripts/player/Knight/KnightFirstPersonAnim.cs
@@ -9,40 +9,35 @@ public class KnightFirstPersonAnim : MonoBehaviour
     // swing stuff
     public void StopSwingAnimPov()
     {
-        gameObject.GetComponent<Animator>().SetBool("IsSwinging" , false);
-        gameObject.GetComponent<Animator>().SetFloat("SwingSpeed", knight.SwingSpeed);
+        
     }
 
     public void ReverseSwingAnimationPov()
     {
-        if(Input.GetMouseButton(0)) gameObject.GetComponent<Animator>().SetFloat("SwingSpeed", -knight.SwingSpeed);
+
     }
 
     public void ResetSwingPov()
     {
-        if(!Input.GetMouseButton(0)) gameObject.GetComponent<Animator>().SetBool("IsSwinging" , false);
-        gameObject.GetComponent<Animator>().SetFloat("SwingSpeed", knight.SwingSpeed);
+
     }
 
     //fireball stuff
     public void SpawnFireballPov()
     {
-        knight.CmdCastFireBall(knight.gameObject.GetComponent<Health>().IsRedTeam);
     }
     public void StopFireBallAnim()
     {
-        gameObject.GetComponent<Animator>().SetBool("Fireball" , false);
         
     }
     //smoke stuff
     public void StopSmokeAnimPov()
     {
-        gameObject.GetComponent<Animator>().SetBool("Smoke" , false);
 
     }
     public void SpawnSmokePov()
     {
-        knight.CmdCastSmoke();
+
     }
 
     //ult stuff
